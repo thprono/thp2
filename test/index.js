206cf5d048c663e5759d1d0f56cfcee960eccfb8
Tae Lim Kook <tkook11@gmail.com>
2015-02-04 17:13:00 -0500

    Update index.js

--- test/index.js
+++ test/index.js
@@ -2,12 +2,12 @@ var mydb = require('../mydb');
 var forum = require('../forum');
 
 exports.createData = function (db) {
-  mydb.createUser(0, 'blue_bear_94', 'password', 'tkook11@gmail.com', 'bluebear94', db);
+  mydb.createUser(0, 'blue_bear_94', 'password', 'something@gmail.com', 'bluebear94', db);
   mydb.admitUser(0, db);
   mydb.givePosition(0, 'Leader', db);
-  mydb.createUser(1, 'THISiSNTDIVYESH', 'password', 'goshthisisveryweird@gmail.com', 'divyeshd', db);
+  mydb.createUser(1, 'THISiSNTDIVYESH', 'password', 'somethingelse@gmail.com', 'divyeshd', db);
   mydb.admitUser(1, db);
-  mydb.createUser(2, 'waterdevil00', 'password', 'something@gmail.com', 'jackdare2', db);
+  mydb.createUser(2, 'waterdevil00', 'password', 'yetsomethingelse@gmail.com', 'jackdare2', db);
   mydb.admitUser(2, db);
   /*forum.request({
     "type": "createThread",
